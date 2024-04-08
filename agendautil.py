008cea52418135f96477d66b8a4324cdccc7defb
Abhijit Dutta <143208793+duttaabh@users.noreply.github.com>
2024-04-08 18:36:05 -0400

    Optimizing the LLM to reduce hallucinations

--- agendautil.py
+++ agendautil.py
@@ -4,6 +4,7 @@ import os
 
 import boto3
 from langchain.chains import RetrievalQA
+from langchain.chains.llm import LLMChain
 from langchain.memory import ConversationBufferWindowMemory
 from langchain_community.chat_models import BedrockChat
 from langchain_community.document_loaders import TextLoader
@@ -30,6 +31,12 @@ embeddings = BedrockEmbeddings(
         # endpoint_url='https://prod.us-west-2.dataplane.bedrock.aws.dev'
     )  # create a Cohere Embeddings client
 
+def convertStringToTime(input):
+    format = '%m/%d/%y %H:%M'
+    import datetime
+    dt = datetime.datetime.strptime(input, format)
+    return str(dt)
+
 #Performing Data Transformation
 def convertSummitDataFromCsvtoJson(csvfilePath, jsonFilePath):
     jsonf = open(jsonFilePath, 'a', encoding='utf-8')
@@ -38,23 +45,10 @@ def convertSummitDataFromCsvtoJson(csvfilePath, jsonFilePath):
         reader = csv.DictReader(f)
         for rows in reader:
             data = {'summitName': rows['\ufeffSummit'] , 'awsSessionID': rows['AWS Session ID'] , 'sessionName': rows['Session name'] , 'sessionAbstract': rows['Abstract'] , 'sessionCapacity': rows['Capacity'],
-                   'sessionDate': rows['Session Date'], 'sessionStartTime': str(rows['Start Time']).replace('1/1/00 ', '') , 'sessionEndTime': str(rows['End Time']).replace('1/1/00 ', ''), 'sessionDuration': rows['Session Duration'], 'sessionTopic': rows['Session Topic'],
+                   'sessionDate': rows['Session Date'], 'sessionStartTime': convertStringToTime(str(rows['Start Time']).replace('1/1/00', rows['Session Date'])) , 'sessionEndTime': convertStringToTime(str(rows['End Time']).replace('1/1/00', rows['Session Date'])), 'sessionDuration': rows['Session Duration'], 'sessionTopic': rows['Session Topic'],
                    'sessionTracks': rows['Session Tracks'], 'sessionType': rows['Session Type'], 'sessionLevel': rows['Session Level'], 'sessionAreaOfInterest': rows['Area of Interest']}
             data_collection.append(data)
     jsonf.write(json.dumps(data_collection, indent=4))
-    jsonf.flush()
-    jsonf.close()
-
-def convertSummitDataFromCsvtoText(csvfilePath, jsonFilePath):
-    jsonf = open(jsonFilePath, 'a', encoding='utf-8')
-    with open(csvfilePath, encoding='utf-8') as f:
-        reader = csv.DictReader(f)
-        for rows in reader:
-            summitName = rows['\ufeffSummit']
-            data = f"summitName: {summitName},awsSessionID:{rows['AWS Session ID']},sessionName: {rows['Session name']},sessionAbstract:{rows['Abstract']},sessionCapacity:{rows['Capacity']},\
-                   sessionDate:{rows['Session Date']},sessionStartTime:{str(rows['Start Time']).replace('1/1/00 ', '')},sessionEndTime:{str(rows['End Time']).replace('1/1/00 ', '')},sessionDuration:{rows['Session Duration']},sessionTopic:{rows['Session Topic']},\
-                   sessionTracks:{rows['Session Tracks']},sessionType:{rows['Session Type']},sessionLevel:{rows['Session Level']},sessionAreaOfInterest:{rows['Area of Interest']}"
-            jsonf.write('{' + data + '}' + '\n')
     jsonf.flush()
     jsonf.close()
 
@@ -86,34 +80,11 @@ def uploadSummitDataToOSS(jsonFilePath, index_name): #creates and returns an in-
     # opensearch_vector_search.add_documents(documents=docs)
     return opensearch_vector_search
 
-def uploadSummitTxtDataToOSS(jsonFilePath, index_name): #creates and returns an in-memory vector store to be used in the application
-    opensearch_vector_search = OpenSearchVectorSearch(
-        opensearch_url = opensearch_endpoint, #"https://5fso0en8s31bts1p2so5.us-east-1.aoss.amazonaws.com",
-        index_name = index_name.lower(),
-        embedding_function = embeddings,
-        http_auth=awsauth,
-        timeout = 300,
-        use_ssl = True,
-        verify_certs = True,
-        connection_class = RequestsHttpConnection
-    )
-
-    with open(jsonFilePath, 'r') as f:
-        for data in f.readlines():
-            with open('test.json', 'w') as testf:
-                    testf.write(data)
-            loader = TextLoader('test.json')
-            documents = loader.load()
-            response = opensearch_vector_search.add_documents(documents=documents)
-            print(f'Document updated with response id {response[0]}')
-        os.remove('test.json')
-    # opensearch_vector_search.add_documents(documents=docs)
-    return opensearch_vector_search
-
 #Setting up the Anthropic Calude 3 Haiku LLM for RAG
 def getLLM():
     model_kwargs_claude = {
-        "temperature": 0.0
+        "temperature": 0.0,
+        "top_k": 250
     }
     llm = BedrockChat(model_id="anthropic.claude-3-haiku-20240307-v1:0",
                   model_kwargs=model_kwargs_claude)
@@ -127,17 +98,23 @@ def getMemory():  # create memory for this chat session
     return memory
 
 
-#Prompt Massaging
-prompt_template = """You are tasked to answer the "{question}" with awsSessionID based on the following context information in html format. Your goal is to generate a final agenda that has no overlapping or conflicting sessions, while strictly adhering to the session Start Time and session End Time provided without adjusting them to fit the agenda based on the following context.
+#Generating Keywords from questions
+def rephraseQuestions(input_text):
+    nysummit_date = '2023-07-26'
+    prompt_template = """Rephrase the {question} as if asking to generate an agenda between a start and end time (if available) in a single sentence. Convert all start or end times to HH:MM:SS format(if available)
+    
+            Keywords:"""
 
-        {context}
+    prompt = PromptTemplate.from_template(prompt_template)
 
-        Failure Instructions:
-        If you are unable to create an agenda that meets the requirements of having no overlapping or conflicting sessions and strictly adhering to the provided session Start and End Times, the task will be considered a failure. In the case of a failure, you should provide a detailed explanation outlining the reasons why the agenda could not be successfully generated within the given constraints.
+    llm_chain = LLMChain(llm=getLLM(), prompt=prompt)
 
-        Question: {question}   
+    response = llm_chain.invoke(input_text, return_only_outputs=False)
 
-        Answer:"""
+    keywords = response['text'].split('\n')[0]
+
+    return keywords
+
 
 #Generating the final agenda using RAG
 def generateAgendaItems(input_text, index_name):
@@ -151,7 +128,29 @@ def generateAgendaItems(input_text, index_name):
             verify_certs=True,
             engine="faiss",
             connection_class=RequestsHttpConnection
-        ).as_retriever(search_type="mmr", search_kwargs={'k': 100, 'lambda_mult': 0.25})
+        ).as_retriever(search_type="mmr", search_kwargs={'k': 10, 'lambda_mult': 0.25})
+    search = OpenSearchVectorSearch(
+        opensearch_url=opensearch_endpoint,
+        index_name=index_name,
+        embedding_function=embeddings,
+        http_auth=awsauth,
+        timeout=300,
+        use_ssl=True,
+        verify_certs=True,
+        engine="faiss",
+        connection_class=RequestsHttpConnection
+    )
+    # Prompt Massaging
+    prompt_template = """{question} with awsSessionID based on the following context information. Do not change the sessionStartTime and sessionEndTime in the generated agenda. Your goal is to generate a final agenda based on the session start time, end time and duration that has no overlapping or conflicting sessions, while strictly adhering to the session Start Time and session End Time provided without adjusting them to fit the agenda based on the following context.
+
+            {context}
+
+            Failure Instructions:
+            If you are unable to create an agenda that meets the requirements of having no overlapping or conflicting sessions and without changing the session Start and End Times, the task will be considered a failure. In the case of a failure, you should provide a detailed explanation outlining the reasons why the agenda could not be successfully generated within the given constraints.
+
+            Question: {question}
+
+            Answer:"""
 
     PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
 
@@ -165,10 +164,43 @@ def generateAgendaItems(input_text, index_name):
     chat_response = qa.invoke(input_text, return_only_outputs=False)
     return chat_response
 
+def factCheckRag(input_text, index_name):
+    retriever = OpenSearchVectorSearch(
+            opensearch_url=opensearch_endpoint,
+            index_name=index_name,
+            embedding_function=embeddings,
+            http_auth=awsauth,
+            timeout=300,
+            use_ssl=True,
+            verify_certs=True,
+            engine="faiss",
+            connection_class=RequestsHttpConnection
+        ).as_retriever(search_type="mmr", search_kwargs={'k': 10, 'lambda_mult': 0.25})
+
+    # Prompt Massaging
+    prompt_template = """You are tasked to check accuracy of sessionStart and sessionEndTime in the generated "{question}" using the following context. Please provide detailed explanation if any of the details are inaccurate.:
+
+            {context}
+
+            Question: {question}   
+
+            Answer:"""
+    PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+
+    qa = RetrievalQA.from_chain_type(llm=getLLM(),
+                                     chain_type="stuff",
+                                     retriever=retriever,
+                                     return_source_documents=True,
+                                     chain_type_kwargs={"prompt": PROMPT, "verbose": True},
+                                     verbose=True)
+
+    chat_response = qa.invoke(input_text, return_only_outputs=False)
+    return chat_response
+
 if __name__ == '__main__':
-    convertSummitDataFromCsvtoJson('session_metadata_newyork_final.csv', 'session_metadata_newyork_final.json')
-    # convertSummitDataFromCsvtoText('session_metadata_newyork_final.csv', 'session_metadata_newyork_final.txt')
-    # uploadSummitTxtDataToOSS('session_metadata_newyork_final.txt', 'ny_summit_session_metadata_txt')
-    # rag_response = generateAgendaItems('I want a 9am-3pm agenda focused in Machine Learning and Big Data. Leave me an hour for lunch', 'ny_summit_session_metadata_txt')
-    # # rag_response = generateAgendaItems('I want a 4 hour agenda including some hands on workshops. Compute and Open Source are most interesting to me.', 'ny_summit_session_metadata_txt')
-    # print(rag_response['result'])
+    question = 'I want a 9am-3pm agenda focused in Machine Learning and Big Data. Leave me an hour for lunch.'
+    # question = 'I want a 4 hour agenda including some hands on workshops. Compute and Open Source are most interesting to me.'
+    rephrasedquestion = rephraseQuestions(question)
+    print(rephrasedquestion)
+    rag_response = generateAgendaItems(rephrasedquestion, 'ny_summit_session_metadata')
+    print(rag_response['result'])
